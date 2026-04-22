7b4d9287d6cff347d1a0d580e5c3173682f7d54b
melek saidani <mbcmelek3@gmail.com>
2026-04-22 22:20:23 +0100

    Update twrp_V2217.mk

--- twrp_V2217.mk
+++ twrp_V2217.mk
@@ -20,9 +20,6 @@ PRODUCT_BRAND := vivo
 PRODUCT_MODEL := V2217
 PRODUCT_MANUFACTURER := vivo
 
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock
