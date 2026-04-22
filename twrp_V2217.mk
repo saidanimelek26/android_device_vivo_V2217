b2cd3318323b619b7aec9cf75b2a04451be692d0
melek saidani <mbcmelek3@gmail.com>
2026-04-22 21:49:07 +0100

    Update and rename omni_V2217.mk to twrp_V2217.mk

---
+++ twrp_V2217.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from V2217 device
+$(call inherit-product, device/vivo/V2217/device.mk)
+
+PRODUCT_DEVICE := V2217
+PRODUCT_NAME := twrp_V2217
+PRODUCT_BRAND := vivo
+PRODUCT_MODEL := V2217
+PRODUCT_MANUFACTURER := vivo
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock
+
+# Bypass anti-rollback ROMs protection
+# Set build date to Jan 1 2009 00:00:00
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.date.utc=1230768000
